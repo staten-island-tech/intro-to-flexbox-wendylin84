eee4db442444fbfae725223c7a39ec60672f13d1
wendylin84 <wendyl84@nycstudents.net>
2025-09-22 14:02:08 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+// // we declare & define variables
+// /* let x = 5;
+// x = 6; */
+// /* const x = 5;
+// console.log(x);
+//  */
+// //primitive
+// //strings represent names/characters/letters ect
+// const x = "Ian";
+// //intergers/numbers
+// const y = 123;
+// //boolean or true/false
+// const citizen = true;
+// //null is the absence of a value
+// let name = null;
+// //undefined
+// let z;
+//functions are input output machines
+//functions accept input or parameters
+//declaring & defining a function
+/* function add(x, y) {
+  //   console.log(x + y);
+  return x + y;
+}
+//run the function
+const bill = add(5, 6);
+console.log(bill);
+ */
+/* const x = 7;
+function returnThing() {
+  //function scope or local scope, only accessible inside this fucntion
+  const x = 8;
+  console.log(x);
+}
+returnThing();
+ */
+//template literals or `` allows us to insert variables into strings
+/* const name = "Nelson";
+console.log(`idk ${name}`);
+ */
+function add(P, B, D) {
+  const badges = Math.floor(p / b);
+  const leftover = p % b;
+  const money = badges * d + leftover;
+  console.log(money);
+}
+add(17, 5, 2);
