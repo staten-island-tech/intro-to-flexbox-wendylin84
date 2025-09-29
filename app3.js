ac1dd55035c000dbf6de9dbec797555006c03d04
wendylin84 <wendyl84@nycstudents.net>
2025-09-29 14:01:01 -0400

    d

---
+++ app3.js
@@ -0,0 +1,38 @@
+/* const students = ["Alice", "Bob", "Charlie", "Davic" /* , 1, null, X, true]; */
+/* students.push("Noah");
+console.log(students); */
+/* console.log(students[0]);
+ */
+/* function printAll(list){
+    for (let i = 0; < students.length; i++){
+        console.log(i)
+        console.log(list[i]);
+    }
+}
+printAll(students); */
+// students.forEach((student) => console.log(student, index));
+/* function add1(a, b) {
+  return a + b;
+}
+const add2 = (a, b) => a + b;
+ */
+
+/* console.log(students.find((student) => student === "Alice")); */
+/* const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbs.filter((numba) => numba % 2 === 0).forEach((numba) => console.log(numba));
+ */
+/* let number = 5;
+while (number > 0) {
+    console.log("coundown: " + number); */
+
+/* const name = "Alice";
+console.log(name[0]);
+ */
+
+function frenchEng(N, lines) {
+    const found = frenchEng.find((N)) => student == "s, S");
+    console.log(found);
+    if ((s && S > t && T))
+        console.log("French");
+     if ((t && T > s && S))
+        console.log("English");
