b9beb78f180e53f6ae960ae9fd135b0f22b7f965
wendylin84 <wendyl84@nycstudents.net>
2025-10-07 14:00:51 -0400

    h

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-// // we declare & define variables
-// /* let x = 5;
-// x = 6; */
-// /* const x = 5;
-// console.log(x);
-//  */
-// //primitive
-// //strings represent names/characters/letters ect
-// const x = "Ian";
-// //intergers/numbers
-// const y = 123;
-// //boolean or true/false
-// const citizen = true;
-// //null is the absence of a value
-// let name = null;
-// //undefined
-// let z;
-//functions are input output machines
-//functions accept input or parameters
-//declaring & defining a function
-/* function add(x, y) {
-  //   console.log(x + y);
-  return x + y;
-}
-//run the function
-const bill = add(5, 6);
-console.log(bill);
- */
-/* const x = 7;
-function returnThing() {
-  //function scope or local scope, only accessible inside this fucntion
-  const x = 8;
-  console.log(x);
-}
-returnThing();
- */
-//template literals or `` allows us to insert variables into strings
-/* const name = "Nelson";
-console.log(`idk ${name}`);
- */
-function add(P, B, D) {
-  const badges = Math.floor(p / b);
-  const leftover = p % b;
-  const money = badges * d + leftover;
-  console.log(money);
-}
-add(17, 5, 2);
+const jewelry = [
+  {
+    name: "Purple, black, & silver decorative bracelet",
+    img: "https://i.ibb.co/h12NFnBS/O1-CN01yue-G6q1-Kj-Bv68d-In-M-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Black & silver decorative bracelet",
+    img: "https://i.ibb.co/Q7M7Mfph/O1-CN01-Jzms0i1-Kj-Bpkr75o-W-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "White, blue, & gold decorative bracelet",
+    img: "https://i.ibb.co/7J2FW0qR/O1-CN01q-C5u-Ef1-Kj-Bps-Tqo-Li-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Light blue decorative bracelet",
+    img: "https://i.ibb.co/TBDzFLj2/O1-CN01pgqfd-S1-Kj-Bpa-Spl-Rj-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Lime decorative bracelet",
+    img: "https://i.ibb.co/wZk0jpTY/O1-CN01e3-Pn-Ug1-Kj-Bp-Wde9ok-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Gold, blue, & purple decorative bracelet",
+    img: "https://i.ibb.co/XxhB6KVp/coc.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Green & gold decorative bracelet",
+    img: "https://i.ibb.co/3w2n13t/O1-CN01c-MB3-Y71-Kj-Bra-Jll-Fw-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Blue & silver decorative bracelet",
+    img: "https://i.ibb.co/GvPRXmrL/O1-CN013-S0-WSd1-Kj-Bq-WAdwk-D-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Yellow & gold decorative bracelet",
+    img: "https://i.ibb.co/jkBgtZXw/O1-CN01-Mnu8-SH1-Kj-Bu-XQa-AIh-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Rose, blue, & pearl decorative bracelet",
+    img: "https://i.ibb.co/Ps2cpwTJ/O1-CN01jw-Kj-VR1-Kj-Bq-Vq-Lp-Sr-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Silver, blue & white decorative bracelet",
+    img: "https://i.ibb.co/yFQvkTYq/O1-CN01bd8-Lv-P1-Kj-Bq-Fw3-Qjj-3973231199.jpg",
+    price: 10.99,
+  },
+  {
+    name: "Red, black, & white decorative bracelet",
+    img: "https://i.ibb.co/twdvGppz/O1-CN01-Ep-Ms731-Kj-Bru8i-Ju-E-3973231199.jpg",
+    price: 10.99,
+  },
+];
+jewelry.forEach((jewelry) => 
