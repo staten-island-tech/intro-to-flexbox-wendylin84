72fb8b15d819a25f293aadac660f31333314533a
wendylin84 <wendyl84@nycstudents.net>
2025-10-08 13:41:18 -0400

    t

--- app.js
+++ app.js
@@ -2,26 +2,31 @@ const jewelry = [
   {
     name: "Purple, black, & silver decorative bracelet",
     img: "https://i.ibb.co/h12NFnBS/O1-CN01yue-G6q1-Kj-Bv68d-In-M-3973231199.jpg",
+    color: "silver",
     price: 10.99,
   },
   {
     name: "Black & silver decorative bracelet",
     img: "https://i.ibb.co/Q7M7Mfph/O1-CN01-Jzms0i1-Kj-Bpkr75o-W-3973231199.jpg",
+    color: "silver",
     price: 10.99,
   },
   {
     name: "White, blue, & gold decorative bracelet",
     img: "https://i.ibb.co/7J2FW0qR/O1-CN01q-C5u-Ef1-Kj-Bps-Tqo-Li-3973231199.jpg",
+    color: "gold",
     price: 10.99,
   },
   {
     name: "Light blue decorative bracelet",
     img: "https://i.ibb.co/TBDzFLj2/O1-CN01pgqfd-S1-Kj-Bpa-Spl-Rj-3973231199.jpg",
+    color: "silver",
     price: 10.99,
   },
   {
     name: "Lime decorative bracelet",
     img: "https://i.ibb.co/wZk0jpTY/O1-CN01e3-Pn-Ug1-Kj-Bp-Wde9ok-3973231199.jpg",
+    color: "gold",
     price: 10.99,
   },
   {
@@ -58,6 +63,46 @@ const jewelry = [
     name: "Red, black, & white decorative bracelet",
     img: "https://i.ibb.co/twdvGppz/O1-CN01-Ep-Ms731-Kj-Bru8i-Ju-E-3973231199.jpg",
     price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
+  },
+  {
+    name:
+img: 
+price: 10.99,
   },
 ];
 jewelry.forEach((jewelry) => 
