12b8e853a0bc0b94d53fff00a355598d14f28f1e
wendylin84 <wendyl84@nycstudents.net>
2025-10-08 14:01:47 -0400

    s

--- app.js
+++ app.js
@@ -2,107 +2,167 @@ const jewelry = [
   {
     name: "Purple, black, & silver decorative bracelet",
     img: "https://i.ibb.co/h12NFnBS/O1-CN01yue-G6q1-Kj-Bv68d-In-M-3973231199.jpg",
-    color: "silver",
+    alt: "Purple, black, & silver decorative bracelet",
+    metal: "silver",
     price: 10.99,
+    inStock: true,
   },
   {
     name: "Black & silver decorative bracelet",
     img: "https://i.ibb.co/Q7M7Mfph/O1-CN01-Jzms0i1-Kj-Bpkr75o-W-3973231199.jpg",
-    color: "silver",
+    alt: "Black & silver decorative bracelet",
+    metal: "silver",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "White, blue, & gold decorative bracelet",
     img: "https://i.ibb.co/7J2FW0qR/O1-CN01q-C5u-Ef1-Kj-Bps-Tqo-Li-3973231199.jpg",
-    color: "gold",
+    alt: "White, blue, & gold decorative bracelet",
+    metal: "gold",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Light blue decorative bracelet",
-    img: "https://i.ibb.co/TBDzFLj2/O1-CN01pgqfd-S1-Kj-Bpa-Spl-Rj-3973231199.jpg",
-    color: "silver",
+    img: "https://i.ibb.co/TBDzFLj2/O1-CN01pgqfd-S1-Kj-Bpa-Spl-Rj-3973231199.jpg",  
+    alt: "Light blue decorative bracelet",
+    metal: "silver",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Lime decorative bracelet",
     img: "https://i.ibb.co/wZk0jpTY/O1-CN01e3-Pn-Ug1-Kj-Bp-Wde9ok-3973231199.jpg",
-    color: "gold",
+    alt: "Lime decorative bracelet",
+    metal: "gold",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Gold, blue, & purple decorative bracelet",
     img: "https://i.ibb.co/XxhB6KVp/coc.jpg",
+    alt: "Gold, blue, & purple decorative bracelet",
+    metal: "gold",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Green & gold decorative bracelet",
     img: "https://i.ibb.co/3w2n13t/O1-CN01c-MB3-Y71-Kj-Bra-Jll-Fw-3973231199.jpg",
+    alt: "Green & gold decorative bracelet",
+   metal: "gold",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Blue & silver decorative bracelet",
     img: "https://i.ibb.co/GvPRXmrL/O1-CN013-S0-WSd1-Kj-Bq-WAdwk-D-3973231199.jpg",
+    alt: "Blue & silver decorative bracelet",
+  metal: "silver",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Yellow & gold decorative bracelet",
     img: "https://i.ibb.co/jkBgtZXw/O1-CN01-Mnu8-SH1-Kj-Bu-XQa-AIh-3973231199.jpg",
+    alt: "Yellow & gold decorative bracelet",
+    metal: "gold",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Rose, blue, & pearl decorative bracelet",
     img: "https://i.ibb.co/Ps2cpwTJ/O1-CN01jw-Kj-VR1-Kj-Bq-Vq-Lp-Sr-3973231199.jpg",
+    alt: "Rose, blue, & pearl decorative bracelet",
+    metal: "silver",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Silver, blue & white decorative bracelet",
     img: "https://i.ibb.co/yFQvkTYq/O1-CN01bd8-Lv-P1-Kj-Bq-Fw3-Qjj-3973231199.jpg",
+    alt: "Silver, blue & white decorative bracelet",
+    metal: "silver",
     price: 10.99,
+     inStock: true,
   },
   {
     name: "Red, black, & white decorative bracelet",
     img: "https://i.ibb.co/twdvGppz/O1-CN01-Ep-Ms731-Kj-Bru8i-Ju-E-3973231199.jpg",
+    alt: "Red, black, & white decorative bracelet",
+    metal: "silver",
     price: 10.99,
+     inStock: true,
+  },
+  {
+    name:
+img:
+alt: 
+metal: 
+price: 10.99,
+ inStock: true,
   },
   {
     name:
 img: 
+alt:
+metal: 
 price: 10.99,
+ inStock: true,
+  },
+  {
+    name:
+img:
+alt: 
+metal: 
+price: 10.99,
+ inStock: true,
+  },
+  {
+    name:
+img:
+alt: 
+metal: 
+price: 10.99,
+ inStock: true,
+  },
+  {
+    name:
+img:
+alt: 
+metal: 
+price: 10.99,
+ inStock: true,
+  },
+  {
+    name:
+img:
+alt: 
+metal: 
+price: 10.99,
+ inStock: true,
   },
   {
     name:
 img: 
+alt:
+metal: 
 price: 10.99,
+ inStock: true,
   },
   {
     name:
-img: 
+img:
+alt: 
+metal: 
 price: 10.99,
-  },
-  {
-    name:
-img: 
-price: 10.99,
-  },
-  {
-    name:
-img: 
-price: 10.99,
-  },
-  {
-    name:
-img: 
-price: 10.99,
-  },
-  {
-    name:
-img: 
-price: 10.99,
-  },
-  {
-    name:
-img: 
-price: 10.99,
+ inStock: true,
   },
 ];
-jewelry.forEach((jewelry) => 
+jewelry.forEach((jewelry) => console.log(jewelry.name, jewelry.metal));
+.filter((jewelry) => jewelry.metal === "gold")
+.filter((jewelry) => jewelry.metal === "silver")
+function inject(jewlery) {
+  .querySelector(".container")
+ .insertAdjacentHTML("afterbegin",
