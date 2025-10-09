c478ea7f1bfa9d1296533be168ac512019b6a940
wendylin84 <wendyl84@nycstudents.net>
2025-10-08 23:56:36 -0400

    s

--- app.js
+++ app.js
@@ -165,4 +165,15 @@ jewelry.forEach((jewelry) => console.log(jewelry.name, jewelry.metal));
 .filter((jewelry) => jewelry.metal === "silver")
 function inject(jewlery) {
   .querySelector(".container")
- .insertAdjacentHTML("afterbegin",
+ .insertAdjacentHTML("afterbegin", "...");
+  function inject(album) {
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="display-card">
+      <img class="display-img" src="${album.url}"/>
+      <h2 class="display-artist">${album.artist}</h2>
+      <h3 class="display-album">${album.title}</h3>
+      <button class="remove btn">Remove Album</button>
+    </div>`
+  );
+}
